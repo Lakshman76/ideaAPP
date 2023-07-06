d403bbf7b1f710b69e14fbd3945ae3fb00b6e137
Lakshman kumar <lakshmankumar2603@gmail.com>
2023-07-06 15:11:54 +0530

    congig server added

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express')
+const serverConfig = require('./config/server.config.js')
 
 const app = express()
 
-app.listen(3033,()=>{
-    console.log("server stated");
+app.listen(serverConfig.PORT,()=>{
+    console.log(`server stated at port ${serverConfig.PORT}`);
 })
